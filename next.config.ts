10a89324e8f69963c1a7de157ebb6d44d8776495
K M Faruk islam <49297348+farukislamyt@users.noreply.github.com>
2026-09-04 01:25:18 +0600

    Remove next-intl Next.js plugin

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
