84b0204f98e72f6daea51df7c65a850a1a0efa2a
K M Faruk islam <49297348+farukislamyt@users.noreply.github.com>
2026-09-03 13:49:09 +0600

    Optimize i18n loading for Bengali-only application

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,19 +1,11 @@
 import { getRequestConfig } from "next-intl/server";
-import { hasLocale } from "next-intl";
-import { routing } from "./routing";
 
-export default getRequestConfig(async ({ requestLocale }) => {
-    const requested = await requestLocale;
-
-    const locale = hasLocale(routing.locales, requested)
-        ? requested
-        : routing.defaultLocale;
-
-    const baseMessages = (await import(`../messages/${locale}.json`)).default;
-    const pageMessages = (await import(`../messages/${locale}-pages.json`)).default;
+export default getRequestConfig(async () => {
+    const baseMessages = (await import("../messages/bn.json")).default;
+    const pageMessages = (await import("../messages/bn-pages.json")).default;
 
     return {
-        locale,
+        locale: "bn",
         messages: {
             ...baseMessages,
             ...pageMessages,
