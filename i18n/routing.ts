1bbd67e62724ebd70eb31a80e155fba895fbc041
K M Faruk islam <49297348+farukislamyt@users.noreply.github.com>
2026-09-03 14:40:57 +0600

    Configure Bengali-only routing without locale prefixes

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
     locales: ["bn"],
     defaultLocale: "bn",
+    localePrefix: "never",
 });
 
 export type Locale = (typeof routing.locales)[number];
